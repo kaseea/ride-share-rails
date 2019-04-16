e1eaab2fc8bb483f7ee3d396b54bfba36efca229
Jessica <jessica.homet@gmail.com>
2019-04-16 11:50:25 -0700

    Create restful routes with resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
-  get 'trips/show'
-  get 'trips/edit'
-  get 'trips/update'
-  get 'trips/create'
-  get 'trips/destroy'
-  get 'drivers/index'
-  get 'drivers/show'
-  get 'drivers/edit'
-  get 'drivers/update'
-  get 'drivers/new'
-  get 'drivers/create'
-  get 'drivers/destroy'
-  get 'passengers/index'
-  get 'passengers/show'
-  get 'passengers/edit'
-  get 'passengers/update'
-  get 'passengers/new'
-  get 'passengers/create'
-  get 'passengers/destroy'
+  root to: "homepages#index"
+  # get 'homepages/index'
+
+  resources :trips
+  resources :drivers
+  resources :passengers
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
