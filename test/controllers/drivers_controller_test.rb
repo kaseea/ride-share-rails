5c5d6bdb911934b4a9ef604a0e213b4057a54d14
Jessica <jessica.homet@gmail.com>
2019-04-17 15:42:39 -0700

    Add tests for drivers controller

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -3,12 +3,23 @@ require "test_helper"
 describe DriversController do
   describe "index" do
     it "can get index" do
-      # Your code here
+      get drivers_path
+      must_respond_with :success
     end
   end
 
   describe "show" do
-    # Your tests go here
+    it "should get show" do
+      get driver_path(Driver.first.id)
+
+      must_respond_with :success
+    end
+
+    it "will respond with 404 if the author is not found" do
+      get driver_path(-1)
+
+      must_redirect_to drivers_path
+    end
   end
 
   describe "edit" do
