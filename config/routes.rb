11943cf41559a8b47131176d663c01f57680a500
Jessica <jessica.homet@gmail.com>
2019-04-18 09:11:59 -0700

    Start nesting routes... not finalized. Routes and/or links will need to be changed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   root to: "homepages#index"
   # get 'homepages/index'
+  resources :trips, only: [:delete, :update, :create, :edit]
 
-  resources :trips
-  resources :drivers
-  resources :passengers
+  # In the instructor example, show, new, and edit are all only in passenger. (also, new button on on passenger in reqs.)
+  # Do we want to do that?
+
+  resources :drivers do
+    resources :trips, only: [:show, :new]
+  end
+
+  resources :passengers do
+    resources :trips, only: [:show, :new]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
