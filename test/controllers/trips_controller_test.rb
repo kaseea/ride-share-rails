4f99118c0218f3bc4f02e180a6d88140b2fa3b29
K <774162+kaseea@users.noreply.github.com>
2019-04-22 10:42:39 -0700

    cleans up trip test with let (finally got it working by using create bang)

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,20 +1,16 @@
 require "test_helper"
 
 describe TripsController do
+  let (:driver) { Driver.create!(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9") }
+  let (:passenger) { Passenger.create!(name: "Nina Hintz Sr.", phone_num: "560.815.3059") }
+  let (:trip) { Trip.create!(driver_id: driver.id, passenger_id: passenger.id, date: DateTime.now, rating: 4, cost: 1234) }
+
   describe "show" do
-
-    # Arrange
     it "should get show" do
-      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-      #Does .create call the method in trips_controller or a ruby method somewhere else? wondering cause I thought I was hardcoding some of the default values
-      trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: DateTime.now, rating: 4, cost: 1234)
-
       get passenger_trip_path(passenger_id: passenger.id, id: trip.id)
 
       must_respond_with :success
     end
-    # Your tests go here
     it "will respond with 404 if the trip is not found" do
       get passenger_trip_path(passenger_id: 1, id: -1)
 
@@ -23,12 +19,7 @@ describe TripsController do
   end
 
   describe "edit" do
-    # Your tests go here
     it "should allow an edit" do
-      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-      trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: DateTime.now, rating: 4, cost: 1234)
-
       get edit_passenger_trip_path(passenger_id: passenger.id, id: trip.id)
 
       must_respond_with :success
@@ -42,10 +33,6 @@ describe TripsController do
 
   describe "update" do
     it "should update trips" do
-      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-      trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: DateTime.now, rating: 4, cost: 1234)
-
       trip_p = {
         trip: {
           id: trip.id,
@@ -63,10 +50,6 @@ describe TripsController do
     end
 
     it "should respond with a bad request if the driver id empty" do
-      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-      trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: DateTime.now, rating: 4, cost: 1234)
-
       trip_p = {
         trip: {
           driver_id: "",
@@ -78,12 +61,6 @@ describe TripsController do
     end
 
     it "should redirect to main page if trip not found" do
-      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-      trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: DateTime.now, rating: 4, cost: 1234)
-
-      # AbstractController::DoubleRenderError: Render and/or redirect were called multiple times in this action. Please note that you may only call render OR redirect, and at most once per action. Also note that neither redirect nor render terminate execution of the action, so if you want to exit an action after redirecting, you need to do something like "redirect_to(...) and return".
-
       trip_p = {
         trip: {},
       }
@@ -96,10 +73,6 @@ describe TripsController do
 
   describe "create" do
     it "should create new trip" do
-      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
-
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-
       expect {
         post passenger_trips_path(passenger_id: passenger.id)
       }.must_change "Trip.count", 1
@@ -119,25 +92,19 @@ describe TripsController do
 
   describe "destroy" do
     it "can delete a trip" do
-      driver = Driver.create(name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9")
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-      trip = Trip.create(driver_id: driver.id, passenger_id: passenger.id, date: DateTime.now, rating: 4, cost: 1234)
+      driver2 = Driver.create(name: "Manny McPooper", vin: "WBWSS52P9NEYLVDE9")
+      passenger2 = Passenger.create(name: "Senor Manny", phone_num: "561.815.3059")
+      trip2 = Trip.create(driver_id: driver2.id, passenger_id: passenger2.id, date: DateTime.now, rating: 4, cost: 1234)
 
       expect {
-
-        # Act
-        delete passenger_trip_path(passenger_id: passenger.id, id: trip.id)
-
-        # Assert
+        delete passenger_trip_path(passenger_id: passenger2.id, id: trip2.id)
       }.must_change "Trip.count", -1
 
       must_respond_with :redirect
-      must_redirect_to passenger_path(passenger.id)
+      must_redirect_to passenger_path(passenger2.id)
     end
 
     it "can give a 404 if trip to be deleted isn't found" do
-      passenger = Passenger.create(name: "Nina Hintz Sr.", phone_num: "560.815.3059")
-
       expect(passenger).must_be_kind_of Passenger
 
       delete passenger_trip_path(passenger_id: passenger.id, id: -1)
